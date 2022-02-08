8ec43c164dc81e34a9f488914b830171dab4483c
barry <habayatou93@gmail.com>
2022-02-08 15:24:27 +0100

    jeu

---
+++ Quiz.js
@@ -0,0 +1,14 @@
+var form1 = document.getElementById("form1")
+var form2 = document.getElementById("form2")
+var bncon = document.getElementById("cnx")
+bncon.onclick = lisfonction.hello
+function lisfonction {
+    hello:()=>{
+      form1.style.visibility = "hidden"
+      form2.style.visibility = "visible"
+
+}
+
+function inscr() {
+
+}
